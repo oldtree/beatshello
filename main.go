7ebe099c5429ab9673d6e3af3c70ac352deebceb
Grapes-tree <treeui.old@gmail.com>
2018-01-13 19:39:29 +0800

    hell python config ,suck it

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/oldtree/beatshello/cmd"
 )
+
 //go build -ldflags=s and go build -ldflags=-w both produce working binaries, which suggests the problem is DWARF-related, as in #23046.
+//pip install --upgrade virtualenv
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
 		os.Exit(1)
